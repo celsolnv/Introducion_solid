bc2bc8d201b0131fcab799c5d93afa9e5c832526
Celso <celsolnv27@gmail.com>
2022-05-21 01:14:20 -0300

    init my project

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,49 @@
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    const user = {
+      name,
+      email,
+    };
+    this.users.push(user);
+
+    return user;
+  }
+
+  findById(id: string): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+
+  findByEmail(email: string): User | undefined {
+    return this.users.find((user) => user.email === email);
+  }
+
+  turnAdmin(receivedUser: User): User {
+    // Complete aqui
+    return receivedUser;
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+
+export { UsersRepository };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+  handle(request: Request, response: Response): Response {}
+}
+
+export { TurnUserAdminController };
